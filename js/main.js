83c3b86dd98bef63cbafdd1626865d597af469dd
kate <k_czarnota@wp.pl>
2023-10-24 20:30:04 +0200

    zadanie 47951

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let myTablet = [7, 13, 21, 24, 47];
+
+function takeFirst(tablica) {
+    return tablica[0];
+}
+
+let first = takeFirst(myTablet);
+console.log("Pierwszy element tablicy: " + first);
